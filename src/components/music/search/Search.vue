787d70721649cec407e1fb1ae94921b971d057c6
wuliann <610575024@qq.com>
2020-02-20 14:33:55 +0800

    修复搜索的bug

--- src/components/music/search/Search.vue
+++ src/components/music/search/Search.vue
@@ -34,6 +34,7 @@
 <script>
 import api from "@/api/index";
 import { searchCat1 } from "@/api/config/search/search";
+import { clearArray } from "@/util/transform";
 import KWapi from "@/api/kuwo/kwIndex";
 import SearchList from "@/components/music/search/SearchList";
 export default {
@@ -42,7 +43,6 @@ export default {
       value: "",
       purifyResult: [],
       hackReset: false, // 第一次result是空的
-      isChange: false
     };
   },
 
@@ -58,135 +58,135 @@ export default {
   methods: {
     onSearch() {
       if (this.type === 1) {
-        if (this.isChange === false) {
-          // const result = []
-          // const r1 = []
-          // const r2 = []
+        // const result = []
+        // const r1 = []
+        // const r2 = []
 
-          // 执行多个并发请求
-          //   this.$axios.all([this.getKWapi(), this.getWYapi()]).then(
-          //     this.$axios.spread((KW, WY) => {
-          //       // console.log(KW.data.data);
+        // 执行多个并发请求
+        //   this.$axios.all([this.getKWapi(), this.getWYapi()]).then(
+        //     this.$axios.spread((KW, WY) => {
+        //       // console.log(KW.data.data);
 
-          //       result.push(KW.data.data)
-          //       result.push(WY.data.data.songs)
+        //       result.push(KW.data.data)
+        //       result.push(WY.data.data.songs)
 
-          //       // 酷我
-          //       for (let i = 0; i < result[0].length; i++) {
-          //         const coarseID = result[0][i].MUSICRID
-          //         const simplifiedID = coarseID.slice(6)
+        //       // 酷我
+        //       for (let i = 0; i < result[0].length; i++) {
+        //         const coarseID = result[0][i].MUSICRID
+        //         const simplifiedID = coarseID.slice(6)
 
-          //         r1.push({
-          //           songName: result[0][i].SONGNAME,
-          //           id: simplifiedID,
-          //           artist: result[0][i].ARTIST,
-          //           picUrl: '',
-          //           api: 'KW'
-          //         })
-          //       }
+        //         r1.push({
+        //           songName: result[0][i].SONGNAME,
+        //           id: simplifiedID,
+        //           artist: result[0][i].ARTIST,
+        //           picUrl: '',
+        //           api: 'KW'
+        //         })
+        //       }
 
-          //       // 网易云
-          //       for (let i = 0; i < result[1].length; i++) {
-          //         r2.push({
-          //           songName: result[1][i].name,
-          //           id: result[1][i].id,
-          //           artist: result[1][i].ar[0].name,
-          //           picUrl: result[1][i].al.picUrl,
-          //           api: 'WY'
-          //         })
-          //       }
+        //       // 网易云
+        //       for (let i = 0; i < result[1].length; i++) {
+        //         r2.push({
+        //           songName: result[1][i].name,
+        //           id: result[1][i].id,
+        //           artist: result[1][i].ar[0].name,
+        //           picUrl: result[1][i].al.picUrl,
+        //           api: 'WY'
+        //         })
+        //       }
 
-          //       // 合并两个数组
-          //       const r3 = r1.concat(r2)
+        //       // 合并两个数组
+        //       const r3 = r1.concat(r2)
 
-          //       // 数组中的对象去重
-          //       const obj = {}
-          //       const r4 = r3.reduce((cur, next) => {
-          //         obj[next.songName]
-          //           ? ''
-          //           : (obj[next.songName] = true && cur.push(next))
-          //         return cur
-          //       }, [])
+        //       // 数组中的对象去重
+        //       const obj = {}
+        //       const r4 = r3.reduce((cur, next) => {
+        //         obj[next.songName]
+        //           ? ''
+        //           : (obj[next.songName] = true && cur.push(next))
+        //         return cur
+        //       }, [])
 
-          //       this.purifyResult.push(r4)
-          //       this.hackReset = true
-          //       this.isChange = true
-          //     })
-          //   )
-          // } else {
-          //   const result = []
-          //   const r1 = []
-          //   const r2 = []
+        //       this.purifyResult.push(r4)
+        //       this.hackReset = true
+        //       this.isChange = true
+        //     })
+        //   )
+        // } else {
+        //   const result = []
+        //   const r1 = []
+        //   const r2 = []
 
-          //   // 清空数组
-          //   const PURIFYRESULT_LENGTH = this.purifyResult.length
-          //   this.purifyResult.splice(0, PURIFYRESULT_LENGTH)
+        //   // 清空数组
+        //   const PURIFYRESULT_LENGTH = this.purifyResult.length
+        //   this.purifyResult.splice(0, PURIFYRESULT_LENGTH)
 
-          //   this.$axios.all([this.getKWapi(), this.getWYapi()]).then(
-          //     this.$axios.spread((KW, WY) => {
-          //       result.push(KW.data.data)
-          //       result.push(WY.data.data.songs)
+        //   this.$axios.all([this.getKWapi(), this.getWYapi()]).then(
+        //     this.$axios.spread((KW, WY) => {
+        //       result.push(KW.data.data)
+        //       result.push(WY.data.data.songs)
 
-          //       // 酷我
-          //       for (let i = 0; i < result[0].length; i++) {
-          //         const coarseID = result[0][i].MUSICRID
-          //         const simplifiedID = coarseID.slice(6)
+        //       // 酷我
+        //       for (let i = 0; i < result[0].length; i++) {
+        //         const coarseID = result[0][i].MUSICRID
+        //         const simplifiedID = coarseID.slice(6)
 
-          //         r1.push({
-          //           songName: result[0][i].SONGNAME,
-          //           id: simplifiedID,
-          //           artist: result[0][i].ARTIST,
-          //           picUrl: '',
-          //           api: 'KW'
-          //         })
-          //       }
+        //         r1.push({
+        //           songName: result[0][i].SONGNAME,
+        //           id: simplifiedID,
+        //           artist: result[0][i].ARTIST,
+        //           picUrl: '',
+        //           api: 'KW'
+        //         })
+        //       }
 
-          //       // 网易云
-          //       for (let i = 0; i < result[1].length; i++) {
-          //         r2.push({
-          //           songName: result[1][i].name,
-          //           id: result[1][i].id,
-          //           artist: result[1][i].ar[0].name,
-          //           picUrl: result[1][i].al.picUrl,
-          //           api: 'WY'
-          //         })
-          //       }
+        //       // 网易云
+        //       for (let i = 0; i < result[1].length; i++) {
+        //         r2.push({
+        //           songName: result[1][i].name,
+        //           id: result[1][i].id,
+        //           artist: result[1][i].ar[0].name,
+        //           picUrl: result[1][i].al.picUrl,
+        //           api: 'WY'
+        //         })
+        //       }
 
-          //       // 合并两个数组
-          //       const r3 = r1.concat(r2)
+        //       // 合并两个数组
+        //       const r3 = r1.concat(r2)
 
-          //       const obj = {}
+        //       const obj = {}
 
-          //       const r4 = r3.reduce((cur, next) => {
-          //         obj[next.songName]
-          //           ? ''
-          //           : (obj[next.songName] = true && cur.push(next))
-          //         return cur
-          //       }, [])
+        //       const r4 = r3.reduce((cur, next) => {
+        //         obj[next.songName]
+        //           ? ''
+        //           : (obj[next.songName] = true && cur.push(next))
+        //         return cur
+        //       }, [])
 
-          //       this.purifyResult.push(r4)
-          //     })
-          //   )
+        //       this.purifyResult.push(r4)
+        //     })
+        //   )
 
-          this.getWYapi().then(res => {
-            const result = res.data.result.songs;
-            const tempArr = [];
-            result.forEach(item => {
-              tempArr.push({
-                songName: item.name,
-                id: item.id,
-                artist: item.artists[0].name,
-                picUrl: item.artists[0].img1v1Url,
-                api: "WY"
-              });
+        // 清除上个搜索数据
+        clearArray(this.purifyResult);
+
+        this.getWYapi().then(res => {
+          const result = res.data.result.songs;
+          const tempArr = [];
+          result.forEach(item => {
+            tempArr.push({
+              songName: item.name,
+              id: item.id,
+              artist: item.artists[0].name,
+              picUrl: item.artists[0].img1v1Url,
+              api: "WY"
             });
-
-            this.purifyResult.push(tempArr);
-
-            this.hackReset = true;
-            this.isChange = true;
           });
-        }
+
+          this.purifyResult.push(tempArr);
+
+          this.hackReset = true;
+        });
       }
     },
 
